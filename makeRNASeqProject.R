40d7e93573407294f461ffb6a44b1c76dfb8d882
Nicholas D. Socci <soccin@mskcc.org>
2020-07-09 13:09:58 -0400

    Added more request fields; write key to tsv file

--- makeRNASeqProject.R
+++ makeRNASeqProject.R
@@ -1,3 +1,6 @@
+len <- function(x) {length(x)}
+cc <- function(...) {paste(...,sep='_')}
+
 args=commandArgs(trailing=T)
 if(len(args)!=1) {
     cat("\n    usage: makeRNASeqProject.R limsRequestFile\n\n")
@@ -8,6 +11,7 @@ source("/home/socci/Code/LIMS/LimsETL/tools.R")
 
 require(yaml)
 require(tidyverse)
+require(openxlsx)
 
 rFile=args[1]
 
@@ -21,6 +25,8 @@ requestRNA=list(
     Institution="bic",
     RunNumber="1",
     NumberOfSamples=mapping %>% distinct(X2) %>% nrow,
+
+    Recipe=request$recipe,
 
     LIMS_Strand = request$strand,
     Strand="Reverse,None",
@@ -39,6 +45,8 @@ requestRNA=list(
     DeliverTo_Name=request$investigatorName,
     DeliverTo_Email=request$investigatorEmail,
 
+    OtherContactEmails=request$otherContactEmails,
+
     Pipelines="NULL, RNASEQ_STANDARD_GENE_V1, RNASEQ_DIFFERENTIAL_GENE_V1",
 
     `Charges-CCFN`=""
@@ -50,3 +58,11 @@ yy=as.yaml(requestRNA)
 yy=gsub("'","",yy)
 write(yy,"_request")
 
+key=mapping %>%
+    mutate(FASTQFileID=gsub(".*/Sample_","",X4)) %>%
+    distinct(FASTQFileID) %>%
+    mutate(InvestigatorSampleID=gsub("_IGO_.*","",FASTQFileID) %>% gsub("-","_",.)) %>%
+    mutate(GroupName="")
+
+write.xlsx(key,gsub("metadata.yaml","sample_key.xlsx",rFile))
+write_tsv(select(key,-GroupName),gsub("metadata.yaml","sample_key.tsv",rFile))
